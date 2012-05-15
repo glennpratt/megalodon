2512d2e5c9300b74c09be5eaf2e3d3f9cd1c8e4f
Glenn Pratt <glennpratt@gmail.com>
2012-05-14 21:36:17 -0500

    Issue #17: Load cookbooks and roles for user directory.  Simplify locating chef-solo.

--- config/solo.rb
+++ config/solo.rb
@@ -10,11 +10,14 @@ file_cache_path    "#{megalodon_root}/cookbooks"
 file_backup_path   "#{megalodon_root}/backup"
 cache_options      ({ :path => "#{megalodon_root}/cache/checksums", :skip_expires => true })
 
-# Optionally store your JSON data file and a tarball of cookbooks remotely.
-#json_attribs "http://chef.example.com/dna.json"
-#recipe_url   "http://chef.example.com/cookbooks.tar.gz"
-cookbook_path File.expand_path(File.join(File.dirname(__FILE__), "..", "cookbooks"))
-role_path     File.expand_path(File.join(File.dirname(__FILE__), "..", "roles"))
+# Load megalodon cookbooks first, then user cookbooks may override.
+cookbook_path [
+  File.expand_path(File.join(File.dirname(__FILE__), "..", "cookbooks")),
+  "#{ENV['HOME']}/.megalodon/cookbooks"
+]
+
+# Load roles and data_bags from user .megalodon directory.
+role_path "#{ENV['HOME']}/.megalodon/roles"
 data_bag_path "#{ENV['HOME']}/.megalodon/data_bags"
 
 Mixlib::Log::Formatter.show_time = false
